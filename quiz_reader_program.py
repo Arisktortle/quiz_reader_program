1f1c5e44b28bd8ad23b52e8a425453e81b655fb7
unknown <dhdbbdhd161@gmail.com>
2025-05-11 19:04:53 +0800

    prompt user to try again, looping untill exit.

--- quiz_reader_program.py
+++ quiz_reader_program.py
@@ -38,17 +38,23 @@ def read_questions(filename="quiz_data.txt"):
     return questions
 
 if __name__ == "__main__":
-    questions = read_questions() #calls the function to read the question in the file, if none the program exits
+    questions = read_questions()
     if not questions:
         exit()
-        
-    question = random.choice(questions) #call a random question for the user to answer
-    print("\n" + question["question"])
-    for opt, text in question["choices"].items():
-        print(f"{opt}) {text}")
 
-    answer = input("\nYour answer (a, b, c, d): ").lower().strip() #prompts the user to input an answer for the random question
-    if answer == question["correct"]: #checks if the answer is correct
-        print("Your Answer is correct.")
-    else:
-        print(f"Your Answer is wrong. The correct answer is {question['correct']}) {question['choices'][question['correct']]}")
+    while True:
+        question = random.choice(questions)
+        print("\n" + question["question"])
+        for opt, text in question["choices"].items():
+            print(f"{opt}) {text}")
+
+        answer = input("\nYour answer (a, b, c, d): ").lower().strip()
+        if answer == question["correct"]:
+            print("Your Answer is correct.")
+        else:
+            print(f"Your Answer is wrong. The correct answer is {question['correct']}) {question['choices'][question['correct']]}")
+
+        try_again = input("\nDo you want to try again? (yes/no): ").lower().strip()
+        if try_again != "yes":
+            print("Thank you for using the Quiz Program")
+            break
